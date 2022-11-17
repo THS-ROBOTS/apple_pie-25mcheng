10ab2f45c04e5399cd69d29ec5efc4253846d4dd
25mcheng <118144109+25mcheng@users.noreply.github.com>
2022-11-17 11:55:31 -0500

    Update applepie.py

--- applepie.py
+++ applepie.py
@@ -20,14 +20,15 @@ Input: 1    Output: 40
 """
 
 class Solution:
-    def apple_pie(pies):
+    def apple_pie(pies, self):
         #TYPE pies: integer (int)
         #RETURN: integer (int)
         
         #TODO: write code to calculate how much fat I gained
         #get rid of the 'None" and return whatever you want to return
-        self = 2(20 * pies)
-        return self
+        self = 1
+        fat = (2(20 * pies)) * self
+        return fat
     
 #PLEASE DON'T TOUCH ANYTHING DOWN HERE :)))
 def main():
